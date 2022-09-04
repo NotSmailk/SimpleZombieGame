e0c334eda5b70b1e8dcd093663c5350484a47472
NotSmailk <smailik959@gmail.com>
2022-09-04 18:38:14 +0400

    04092022

--- SimpleZombieGame/Assets/Scripts/Player/PlayerAnimator.cs
+++ SimpleZombieGame/Assets/Scripts/Player/PlayerAnimator.cs
@@ -6,21 +6,15 @@ public class PlayerAnimator : MonoBehaviour
 
     private int Idle = Animator.StringToHash("PlayerIdle");
     private int Death = Animator.StringToHash("Death");
-    private int StrafeLeft = Animator.StringToHash("PlayerStrafeLeft");
-    private int StrafeRight = Animator.StringToHash("PlayerStrafeRight");
 
     private void Awake()
     {
         m_animator = GetComponentInChildren<Animator>();
     }
 
-    public void SetState(int leftValue, int rightValue)
+    private void Start()
     {
-        int value = leftValue - rightValue;
-
-        int state = value == 0 ? Idle : value > 0 ? StrafeLeft : StrafeRight;
-
-        m_animator.CrossFade(state, 0, 0);
+        m_animator.CrossFade(Idle, 0, 0);
     }
 
     public void PlayDeath()
